fb923979765cbae932c7659b57c935e7cfa3113b
Quoc Dat Ngo <a1842806@ing234w021.ad.adelaide.edu.au>
2023-03-24 16:29:47 +1030

    last

--- r2.cpp
+++ r2.cpp
@@ -1,7 +1,9 @@
-#include <iostream>
-
 void stars(int n) {
-    if (n == 0) return;
+    if (n == 0) 
+    {
+        std::cout << std::endl;
+        return;
+    }
     std::cout << '*';
     stars(n - 1);   
 }
