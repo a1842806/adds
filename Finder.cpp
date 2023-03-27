774f8935e75101d142b20cca774ac32501d84943
datr1bs <datngo512@gmail.com>
2023-03-27 18:08:07 +1030

    v2

--- Finder.cpp
+++ Finder.cpp
@@ -5,22 +5,25 @@
     vector<int> Finder::findSubstrings(string s1, string s2) {
 
         vector<int> result;
-        int found = -1; 
+        bool notFound = false;
 
         // tmp represents the prefixes of s2
-        string tmp = "";
+        string tmp = s2.substr(0, 1);
+        size_t found = s1.find(tmp); 
+        result.push_back(found);
     
-        for(int i = 1; i <= s2.size(); i++) {
-            tmp += s2[i - 1];
-            if (found != -1 && s1[found + 1] == tmp.back()) { 
-                result.push_back(found);
+        for(size_t i = 1; i < s2.size(); i++) {
+            if (notFound) {
+                result.push_back(-1);
                 continue;
-            } 
-            found = s1.find(tmp, found + 1);
+            }
+            tmp += s2[i];
+            found = s1.find(tmp, found);
             if (found != string::npos) {
                 result.push_back(found);
             } else {
                 result.push_back(-1);
+                notFound = true;
             }
         }
         return result;
