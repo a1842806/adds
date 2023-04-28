bfc755302e01a84e6c97d0cb039e087ceb9c8efa
datr1bs <datngo512@gmail.com>
2023-04-28 20:43:01 +0930

    v3

---
+++ BigNumCalc.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <list>
+
+class BigNumCalc {
+public:
+    std::list<int> buildBigNum(std::string numString);
+    std::list<int> add(std::list<int> num1, std::list<int> num2);
+    std::list<int> sub(std::list<int> num1, std::list<int> num2);
+    std::list<int> mul(std::list<int> num1, std::list<int> num2);
+private:
+};
--- Makefile
+++
@@ -1,4 +0,0 @@
-main: Node.cpp LinkedList.cpp mainA-1.cpp
-	rm -f main
-	g++ Node.cpp LinkedList.cpp mainA-1.cpp -o main
-	./main
